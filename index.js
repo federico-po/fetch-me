70ba3267a659f032d63eaaab0e703e1e601f6377
Federico Ponce de Leon <fm.poncedeleon@gmail.com>
2022-01-06 16:13:28 -0300

    upgrade3

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const server = require("./server");
 const PORT = process.env.PORT ?? 3000;
